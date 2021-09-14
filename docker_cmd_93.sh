92143353c4519010c6b2f14ab1b9f360bd614a94
93 machine <155051843@qq.com>
2021-09-14 10:07:02 +0800

    code ok

--- docker_cmd_93.sh
+++ docker_cmd_93.sh
@@ -1,8 +1,11 @@
-img="tensorflow/tensorflow:2.5.0-gpu"
+#img="tensorflow/tensorflow:2.5.0-gpu"
+img="nvcr.io/nvidia/pytorch:20.08-py3"
 #img="padim:0.1"
 
-docker run --privileged=true   --workdir /git --name "nlpaug"  -e DISPLAY --ipc=host -d --rm  -p 2203:4452  \
--v /raid/git/nlpaug:/git/nlpaug $img sleep infinity
 
-docker exec -it nlpaug  /bin/bash
+nvidia-docker run --privileged=true   --workdir /git --name "nlpaug"  -e DISPLAY --ipc=host -d --rm  -p 2203:7899  \
+-v /raid/git/nlpaug:/git/nlpaug \
+-v /raid/git/datasets:/git/datasets \
+$img sleep infinity
 
+nvidia-docker exec -it nlpaug  /bin/bash
--- start_jupyter.sh
+++ start_jupyter.sh
@@ -1,3 +1,3 @@
 jupyter notebook --generate-config
 jupyter notebook password
-jupyter-lab --allow-root --ip 0.0.0.0  --port 8899
+jupyter-lab --allow-root --ip 0.0.0.0  --port 7899
